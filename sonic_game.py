7c2539e7ac4a55b82d92c21552bb0b70d67f653b
hyunsuk01 <hyunsuk0109@naver.com>
2024-10-19 20:26:57 +0900

    백그라운드 및 맵 이미지 출력

--- sonic_game.py
+++ sonic_game.py
@@ -10,7 +10,7 @@ map = load_image('map.png')
 character = load_image('sonic_sheet.png')
 
 def handle_events():
-    global running
+    global running, dir
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -18,14 +18,35 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 running = False
+            elif event.key == SDLK_RIGHT:
+                dir = 1
+            elif event.key == SDLK_LEFT:
+                dir = -1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir = 0
+            elif event.key == SDLK_LEFT:
+                dir = 0
 
 running = True
+dir = 0
+
 frame = 0
 char_x = 25
 char_y = Height // 2
+char_speed = 5
+
+bg_x = 0
+bg_speed = 3840 / 5120 #background 이미지 가로 / map 이미지 가로
+
+map_x = 0
+map_y = 100
+map_speed = 5
 
 while running:
     clear_canvas()
+    background.clip_draw(int(bg_x), 0, Width, Height, Width // 2, Height // 2)
+    map.clip_draw(map_x, map_y, Width, Height, Width // 2, Height // 2)
     character.clip_draw(frame, 50 * 4, 50, 50, char_x, char_y)
     update_canvas()
     handle_events()
